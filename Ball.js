ea324cc06e0ad0b255fac0509a58557f735a6528
Daffa <daffaputradamar@gmail.com>
2018-03-16 17:29:12 +0700

    initial commit

---
+++ Ball.js
@@ -0,0 +1,7 @@
+function Ball(){
+    this.x,
+    this.y,
+    this.diameter,
+    this.Xspeed,
+    this.Yspeed,
+}
---
+++ sketch.js
@@ -0,0 +1,67 @@
+var circle = {
+	x : 0,
+	y : 180,
+	diameter : 50,
+	Xspeed : 4,
+	Yspeed : -5
+};
+
+var klikMouse = false;
+
+colorBG ={
+	r : 0,
+	g : 255,
+	b : 255
+}
+
+function setup() {
+	createCanvas(640, 360);
+}
+
+function draw() {
+	colorBG.r = map(circle.x, 0, width, 0, 255);
+	colorBG.g = map(circle.x, 0, width, 255, 255);
+	colorBG.b = map(circle.x, 0, width, 255, 0);
+	if (klikMouse) {
+		background(colorBG.r,colorBG.g,colorBG.b);		
+		showCircle();
+		bounce();
+		move();
+		
+	} else {
+		background(colorBG.r,colorBG.g,colorBG.b);
+		showCircle();
+	}	
+}
+
+function showCircle() {
+		noStroke();
+		fill(240, 155, 60);
+		ifInsideCircle();
+	ellipse(circle.x, circle.y, circle.diameter, circle.diameter);
+}
+
+function ifInsideCircle(){
+	if ((mouseX >= circle.x-(circle.diameter/2) && mouseX <= (circle.diameter/2)+circle.x)
+		&& (mouseY >= circle.y-(circle.diameter/2) && mouseY <= (circle.diameter/2)+circle.y)) {
+		fill(200,0,100);
+	}
+}
+
+function bounce() {
+	if (circle.x > width || circle.x < 0 ) {
+		circle.Xspeed = circle.Xspeed * -1;
+	}
+	if (circle.y > height || circle.y < 0) {
+		circle.Yspeed = circle.Yspeed * -1;			
+	}
+}
+
+function move() {
+	circle.x = circle.x + circle.Xspeed;
+	circle.y = circle.y + circle.Yspeed;
+}
+
+function mouseClicked(){
+	klikMouse = !klikMouse
+}
